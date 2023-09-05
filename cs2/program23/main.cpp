8a9a5c39c8b395532df01eba0af2f8377df7fbc2
yeagerhaus <designedbyyeager@gmail.com>
2023-09-04 19:48:27 -0500

    add p23

---
+++ cs2/program23/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <climits>
+#include "heap.h"
+#include "heap.cpp"
+
+using namespace std;
+
+
+int main()
+{
+    MinHeap h(11);
+    h.insertKey(3);
+    h.insertKey(2);
+    h.deleteKey(1);
+    h.insertKey(15);
+    h.insertKey(5);
+    h.insertKey(4);
+    h.insertKey(45);
+    cout << h.extractMin() << " ";
+    cout << h.getMin() << " ";
+    h.decreaseKey(2, 1);
+    cout << h.getMin();
+    return 0;
+}
